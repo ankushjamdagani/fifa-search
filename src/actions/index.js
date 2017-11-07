6c91528913da3886a32e66f6c7dd817855e03b50
ankushjamdagani <anqush.jamdagani@gmail.com>
2017-11-08 05:07:58 +0530

    actions setter

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,39 @@
+const searchBegin = (payload) => ({
+	type: 'SEARCH_BEGIN',
+	payload
+})
 
+const searchError = (payload) => ({
+	type: 'SEARCH_ERROR',
+	payload
+})
+
+const searchSuccess = (payload) => ({
+	type: 'SEARCH_SUCCESS',
+	payload
+})
+
+const searchByCountry = (countryStr) => {
+	return (dispatch) => {
+		dispatch(searchBegin());
+
+		fetch('http://worldcup.sfg.io/matches/country?fifa_code=' + countryStr)
+		.then(resp => resp.json())
+		.then(resp => {
+			if(resp.ok)
+				dispatch(searchSuccess(resp));
+			else
+				dispatch(searchError(err));
+		})
+		.catch(err => {
+			dispatch(searchError(err));
+		})
+	}
+}
 
 export default {
-
+	searchBegin,
+	searchError,
+	searchSuccess,
+	searchByCountry	
 }
--- src/app.jsx
+++ src/app.jsx
@@ -2,13 +2,53 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import actions from './actions';
-console.log(actions);
 
-const App = () => (
-	<div className="fifa-search">
-		This is FIFA SEARCH
+const App = ({loading, error, results}) => {
+	return <div className="fifa-search">
+		<div className="search-form-wrapper">
+			<form className="search-form">
+				<i className="fa fa-search" />
+				<input type="text" placeholder="Search countries by FIFA code..." />
+				<input type="submit" value="Search" />
+			</form>
+		</div>
+		<div className="search-result-wrapper">
+			{
+				loading && (
+					<div className="loader-wrapper">
+						<div className="loader-circle">
+						</div>
+					</div>
+				)
+			}
+			{
+				!loading && error && (
+					<div className="error-wrapper">
+						<div className="status-code">
+							{ error.statusCode }
+						</div>
+						<div className="status-text">
+							{ error.statusText }
+						</div>
+					</div>
+				)
+			}
+			{
+				!loading && !error && (
+					<div className="search-result-wrapper">
+						{
+							results && results.map((item,i) => {
+								<li key={i}>
+									{ item.location }
+								</li>
+							})
+						}
+					</div>
+				)
+			}
+		</div>
 	</div>
-);
+};
 
 const mapStateToProps = (state, props) => {
 	return {
@@ -19,7 +59,7 @@ const mapStateToProps = (state, props) => {
 
 const mapDispatchToProps = (dispatch, props) => {
 	return {
-		// ....dispatch
+		searchByCountry: dispatch(actions.searchByCountry)
 	}
 }
 
